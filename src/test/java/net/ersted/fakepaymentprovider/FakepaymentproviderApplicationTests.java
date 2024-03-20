5f3017a2cc53b58ad28ee6ca27383252abfb027d
brudko <brudko@bivgroup.ru>
2024-03-20 16:43:30 +0300

    create project with dependency and application config

---
+++ src/test/java/net/ersted/fakepaymentprovider/FakepaymentproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ersted.fakepaymentprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakepaymentproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
